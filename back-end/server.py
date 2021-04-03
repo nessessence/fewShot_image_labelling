b55a227bcb48947b55a9d4b782c40a22ff4ad579
Chavin Chuangchaichatchavarn <cc6chavin@gmail.com>
2021-04-03 18:15:36 +0700

    feat(projects): GET /projects api

--- back-end/server.py
+++ back-end/server.py
@@ -3,11 +3,14 @@ from mongo import MongoAPI
 
 app = Flask(__name__)
 mongo_url = "mongodb://localhost:5000/"
+
+
 def generate_connection_config(collection):
     return {
         "database": "NoMoreLabel",
         "collection": collection
     }
+
 
 @app.route('/')
 def base():
@@ -19,15 +22,32 @@ def base():
 
 # absolute path of the test folder :
 # /c/Users/HP/Desktop/root-test
+
+
 @app.route('/projects', methods=['GET'])
-def get_all_project():
+def get_project():
+    project_id = request.args.get('project_id')
     mongo_obj = MongoAPI(generate_connection_config('projects'), mongo_url)
-    response = mongo_obj.read()
-    return Response(
-        response=json.dumps(response),
-        status=200,
-        mimetype='application/json'
-    )
+    if project_id == None:
+        response = mongo_obj.read()
+        return Response(
+            response=json.dumps(response),
+            status=200,
+            mimetype='application/json'
+        )
+    else:
+        response = mongo_obj.find_one({"project_id": project_id})
+        return Response(
+            response=json.dumps(response),
+            status=200,
+            mimetype='application/json'
+        )
+
+
+@app.route('/projects', methods=['POST'])
+def read_project_folder():
+    print('post')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001, host='0.0.0.0')
